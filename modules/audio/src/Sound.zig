cff79fd8850e74eea7e91824fdfa9cfabf3e8c6f
hazeycode <22148308+hazeycode@users.noreply.github.com>
2022-04-17 00:29:29 +0100

    audio: implement basic stereo mixer

---
+++ modules/audio/src/Sound.zig
@@ -0,0 +1,57 @@
+const std = @import("std");
+
+const Sound = @This();
+
+pub const Priority = enum { low, high };
+
+ptr: *anyopaque,
+vtable: *const VTable,
+channels: u16,
+priority: Priority,
+
+pub const SampleFn = fn (*anyopaque, u32, []f32) usize;
+
+pub const VTable = struct {
+    sample: SampleFn,
+};
+
+pub fn init(
+    pointer: anytype,
+    comptime sample_fn: SampleFn,
+    channels: u16,
+    priority: Priority,
+) Sound {
+    const Ptr = @TypeOf(pointer);
+    const ptr_info = @typeInfo(Ptr);
+
+    std.debug.assert(ptr_info == .Pointer); // Must be a pointer
+    std.debug.assert(ptr_info.Pointer.size == .One); // Must be a single-item pointer
+
+    const alignment = ptr_info.Pointer.alignment;
+
+    const gen = struct {
+        fn sampleImpl(ptr: *anyopaque, sample_rate: u32, buffer: []f32) usize {
+            const self = @ptrCast(Ptr, @alignCast(alignment, ptr));
+            return @call(
+                .{ .modifier = .always_inline },
+                sample_fn,
+                .{ self, sample_rate, buffer },
+            );
+        }
+
+        const vtable = VTable{
+            .sample = sampleImpl,
+        };
+    };
+
+    return .{
+        .ptr = pointer,
+        .vtable = &gen.vtable,
+        .channels = channels,
+        .priority = priority,
+    };
+}
+
+pub inline fn sample(self: *Sound, sample_rate: u32, buffer: []f32) usize {
+    return self.vtable.sample(self.ptr, sample_rate, buffer);
+}
--- modules/audio/src/main.zig
+++ modules/audio/src/main.zig
@@ -1,5 +1,179 @@
 const std = @import("std");
 
+const log = std.log.scoped(.@"brucelib.audio");
+
+const Sound = @import("Sound.zig");
+
+const sin = std.math.sin;
+const pi = std.math.pi;
+const tao = 2 * pi;
+
+pub const SineWave = struct {
+    freq: f32,
+    cursor: usize = 0,
+
+    pub fn sound(self: *@This(), priority: Sound.Priority) Sound {
+        return Sound.init(@ptrCast(*anyopaque, self), @This().sample, 1, priority);
+    }
+
+    pub fn sample(ptr: *anyopaque, sample_rate: u32, buffer: []f32) usize {
+        const self = @ptrCast(*SineWave, @alignCast(@alignOf(*SineWave), ptr));
+
+        for (buffer) |*s, i| {
+            s.* = 0.5 * sin(
+                @intToFloat(f32, self.cursor + i) * (tao * self.freq) / @intToFloat(f32, sample_rate),
+            );
+        }
+        self.cursor += buffer.len;
+        return buffer.len;
+    }
+};
+
+/// Provides an interface for mixing multiple sounds together into an output buffer
+// TODO(hazeycode): support for channel configs other than stereo
+pub const Mixer = struct {
+    // TODO(hazeycode): comptime Mixer params
+    const num_inputs = 16;
+    const input_buf_len = 2048;
+
+    inputs: [num_inputs]Input,
+    mutex: std.Thread.Mutex = .{},
+
+    /// Represents an input channel that can be mixed with others
+    const Input = struct {
+        sound: ?Sound = null,
+        gain: f32 = 0.67, // 0.0...1.0
+        sends: [2]f32 = .{ 1.0, 1.0 }, // 0.0...1.0
+        buffer: [input_buf_len]f32 = .{0} ** input_buf_len,
+    };
+
+    pub fn init() @This() {
+        var res: @This() = .{ .inputs = undefined };
+        for (res.inputs) |*input| input.* = .{};
+        return res;
+    }
+
+    /// Play a sound
+    /// Binds the sound to a free input channel or overrides a lower priority one
+    /// Will be unbound if&when `sample_fn` writes no samples or when the user calls `stop` or `stopAll`
+    /// Returns the input channel index that was bound or null if sound can't be played
+    pub fn play(self: *@This(), sound_source: anytype, priority: Sound.Priority) ?u32 {
+        std.debug.assert(@typeInfo(@TypeOf(sound_source)) == .Pointer);
+
+        if (self.bindInput(sound_source, priority)) |input_channel_idx| {
+            return input_channel_idx;
+        } else {
+            log.warn("No free mixer channels to play sound", .{});
+            std.debug.assert(priority != .high);
+        }
+        return null;
+    }
+
+    /// Stop a playing sound by input channel index
+    pub fn stop(self: *@This(), channel: u32) void {
+        self.mutex.lock();
+        defer self.mutex.unlock();
+
+        self.inputs[channel].sound = null;
+        log.debug("Stopped playing sound on mixer channel {}", .{channel});
+    }
+
+    /// Stop all playing sounds
+    pub fn stopAll(self: *@This()) void {
+        var i: usize = 0;
+        while (i < self.inputs.len) : (i += 1) {
+            self.stop(i);
+        }
+    }
+
+    /// Mixes all input channel and write to the given buffer
+    pub fn mix(
+        self: *@This(),
+        num_frames: u32,
+        out_channels: u32,
+        sample_rate: u32,
+        sample_buf: []f32,
+    ) void {
+        self.mutex.lock();
+        defer self.mutex.unlock();
+
+        var frames_remaining: u32 = num_frames;
+        while (frames_remaining > 0) {
+
+            // buffer up input samples
+            for (self.inputs) |*input, i| {
+                if (input.sound) |*sound| {
+                    const num_samples = sound.sample(
+                        sample_rate,
+                        input.buffer[0..num_frames],
+                    );
+
+                    // unbind if no samples were written
+                    if (num_samples == 0) {
+                        stop(self, @intCast(u32, i));
+                    }
+                }
+            }
+
+            const frames = num_frames;
+
+            var n: u32 = 0;
+            while (n < frames) : (n += 1) {
+                var frame_samples: [2]f32 = .{ 0, 0 };
+
+                for (self.inputs) |input| {
+                    if (input.sound) |sound| {
+                        switch (sound.channels) {
+                            1 => {
+                                frame_samples[0] += input.sends[0] * input.gain * input.buffer[n];
+                                frame_samples[1] += input.sends[1] * input.gain * input.buffer[n];
+                            },
+                            2 => {
+                                frame_samples[0] += input.sends[0] * input.gain * input.buffer[n * 2 + 0];
+                                frame_samples[1] += input.sends[1] * input.gain * input.buffer[n * 2 + 1];
+                            },
+                            else => std.debug.panic("Unsupported number of channels: {}", .{sound.channels}),
+                        }
+                    }
+                }
+
+                sample_buf[n * out_channels + 0] = frame_samples[0];
+                sample_buf[n * out_channels + 1] = frame_samples[1];
+            }
+
+            frames_remaining -= frames;
+        }
+    }
+
+    /// Binds the sound to a free mixer channel or overrides a lower priority one
+    /// Will be unbound if&when `sample_fn` writes no samples
+    /// Returns the mixer channel index that was bound
+    fn bindInput(self: *@This(), sound_source: anytype, priority: Sound.Priority) ?u32 {
+        std.debug.assert(@typeInfo(@TypeOf(sound_source)) == .Pointer);
+
+        self.mutex.lock();
+        defer self.mutex.unlock();
+
+        for (self.inputs) |*input, i| {
+            if (input.sound == null) {
+                input.sound = sound_source.sound(priority);
+                return @intCast(u32, i);
+            }
+        }
+        // If the sound is high priority, see if there is a lower priority sound
+        // that we can it swap for
+        if (priority == .high) {
+            for (self.inputs) |*input, i| {
+                if (input.sound.?.priority == .low) {
+                    input.sound = sound_source.sound(priority);
+                    return @intCast(u32, i);
+                }
+            }
+        }
+        return null;
+    }
+};
+
 test {
     std.testing.refAllDecls(@This());
 }
