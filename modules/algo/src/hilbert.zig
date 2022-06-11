f0bd3e082405c8660548f2074746cab30aed64fb
hazeycode <22148308+hazeycode@users.noreply.github.com>
2022-06-11 22:45:19 +0100

    algo: benchmark hilbert sort

--- modules/algo/src/hilbert.zig
+++ modules/algo/src/hilbert.zig
@@ -4,6 +4,8 @@
 const std = @import("std");
 const testing = std.testing;
 
+const benchmark = @import("bench").benchmark;
+
 
 pub const Point = [2]u32;
 
@@ -14,7 +16,6 @@ pub const Point = [2]u32;
 /// Also credit to Mohamed Badawy for clarifying typos in the pseudo code and providing working examples:
 /// https://stackoverflow.com/a/66994116/16481771
 // TODO(hazeycode): Compare this recursive version to an interative method
-// TODO(hazeycode): measure & optimise
 // TODO(hazeycode): 3d variant or generalise
 pub fn sort_points_2d(args: struct {
     points: []Point,
@@ -173,4 +174,83 @@ test "hilbert sort" {
 }
 
 
+// Benchmarks ===============================================================================================
+
+fn random_points(allocator: std.mem.Allocator, count: usize) ![]Point {
+    var points = try allocator.alloc(Point, count);
+
+    var rng = std.rand.DefaultPrng.init(0);
+    for (points) |*p| p.* = .{
+        rng.random().int(u32),
+        rng.random().int(u32),
+    };
+
+    return points;
+}
+
+test "benchmark hilbert sort points 2d" {
+    var temp_arena = std.heap.ArenaAllocator.init(testing.allocator);
+    defer temp_arena.deinit();
+    
+    var allocator = temp_arena.allocator();
+    
+    try benchmark(
+        &struct {
+            data: [16]struct { order: u32, points: []Point },
+        }{
+            .data = .{
+                .{ .order = 1, .points = try random_points(allocator, 64) },
+                .{ .order = 1, .points = try random_points(allocator, 256) },
+                .{ .order = 1, .points = try random_points(allocator, 1024) },
+                .{ .order = 1, .points = try random_points(allocator, 4096) },
+                .{ .order = 2, .points = try random_points(allocator, 64) },
+                .{ .order = 2, .points = try random_points(allocator, 256) },
+                .{ .order = 2, .points = try random_points(allocator, 1024) },
+                .{ .order = 2, .points = try random_points(allocator, 4096) },
+                .{ .order = 3, .points = try random_points(allocator, 64) },
+                .{ .order = 3, .points = try random_points(allocator, 256) },
+                .{ .order = 3, .points = try random_points(allocator, 1024) },
+                .{ .order = 3, .points = try random_points(allocator, 4096) },
+                .{ .order = 4, .points = try random_points(allocator, 64) },
+                .{ .order = 4, .points = try random_points(allocator, 256) },
+                .{ .order = 4, .points = try random_points(allocator, 1024) },
+                .{ .order = 4, .points = try random_points(allocator, 4096) },
+            },
+        },
+        struct {
+            const range = @import("comptime_range.zig").range;
+            pub const args = range(0, 15);
+        
+            pub const arg_names = [_][]const u8 {
+                "64 random points, order 1",
+                "256 random points, order 1",
+                "1024 random points, order 1",
+                "4096 random points, order 1",
+                "64 random points, order 2",
+                "256 random points, order 2",
+                "1024 random points, order 2",
+                "4096 random points, order 2",
+                "64 random points, order 3",
+                "256 random points, order 3",
+                "1024 random points, order 3",
+                "4096 random points, order 3",
+                "64 random points, order 4",
+                "256 random points, order 4",
+                "1024 random points, order 4",
+                "4096 random points, order 4",
+            };
+        
+            pub fn bench_hilbert_sort_points_2d(ctx: anytype, comptime data_idx: usize) ![]Point {
+                var axis: u32 = 0;
+                sort_points_2d(.{
+                    .points = ctx.data[data_idx].points,
+                    .end = @intCast(i32, ctx.data[data_idx].points.len - 1),
+                    .order = ctx.data[data_idx].order,
+                    .axes_start = &axis,
+                });
+                return ctx.data[data_idx].points;
+            }
+        }
+    );
+}
 
